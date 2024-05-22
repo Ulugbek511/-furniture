03afe23c6ad2ccb62286c4d85d12365024816fd7
Ulugbek <khanjarovulugbek77@gmail.com>
2024-05-23 01:26:35 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import AdminPage from './pages/AdminPage';
-import ClientPage from './pages/ClientPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './componetes/pages/LoginPage';
+import AdminPage from './componetes/pages/AdminPage';
+import ClientPage from './componetes/pages/ClinetPage';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={LoginPage} />
-        <Route path="/admin" component={AdminPage} />
-        <Route path="/client" component={ClientPage} />
-      </Switch>
+      <Routes>
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/client" element={<ClientPage />} />
+      </Routes>
     </Router>
   );
 }
--- src/componetes/pages/LoginPage.jsx
+++ src/componetes/pages/LoginPage.jsx
@@ -1,24 +1,21 @@
-
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      // Backendga so'rov yuborish
       const response = await axios.post('https://ecommerce-backend-fawn-eight.vercel.app/login', {
         username,
         password,
       });
-      // Javob muvaffaqiyatli bo'lsa, admin sahifasiga yo'naltirish
       if (response.status === 200) {
-        history.push('/admin');
+        navigate('/admin');
       }
     } catch (error) {
       console.error('Login xato', error);
